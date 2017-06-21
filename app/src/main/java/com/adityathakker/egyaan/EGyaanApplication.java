4d45fb75664afcce5f64d93f095e81be17ce38ac
Aditya Thakker <adityathakker02@gmail.com>
2017-06-21 17:16:47 +0530

    Login and Home added

---
+++ app/src/main/java/com/adityathakker/egyaan/EGyaanApplication.java
@@ -0,0 +1,23 @@
+package com.adityathakker.egyaan;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by Aditya Thakker (Github: @adityathakker) on 24/12/16.
+ */
+
+public class EGyaanApplication extends Application {
+
+    @Override public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        // Normal app init code...
+    }
+}
